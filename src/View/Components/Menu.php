e7a81c933cfd055667f91546c0c03fd89cce3da7
salahhusa9 <salahhusa9@gmail.com>
2023-03-17 15:58:24 +0100

    typing $menu in components

--- src/View/Components/Menu.php
+++ src/View/Components/Menu.php
@@ -4,10 +4,11 @@ namespace Salahhusa9\Menu\View\Components;
 
 use Illuminate\View\Component;
 use Salahhusa9\Menu\Facades\Menu as FacadesMenu;
+use Salahhusa9\Menu\Menu as MenuMenu;
 
 class Menu extends Component
 {
-    public FacadesMenu $menu;
+    public MenuMenu $menu;
 
     public function __construct()
     {

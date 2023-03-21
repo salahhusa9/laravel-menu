96b7db87c77cd771ab9ce173de5514de2cf46866
salahhusa9 <salahhusa9@gmail.com>
2023-03-21 19:26:32 +0000

    comments of configs

--- README.md
+++ README.md
@@ -98,6 +98,29 @@ You can publish the config file with:
 php artisan vendor:publish --tag="menu-config"
 ```
 
+and you can change the defult menu Classes of Ul and Li and the active class
+
+```php
+return [
+    "ul_class" => "menu-inner py-1", // default menu class
+    "ul_sub_menu_class" => "menu-sub", // default submenu class
+
+    "li_class" => "menu-item", // default menu item class
+    "li_sub_menu_class" => "menu-item", // default submenu item class
+    "li_sub_menu_open_class" => "menu-item active open", // default submenu item class when open
+
+    "a_class" => "menu-link", // default menu link class
+    "a_sub_menu_class" => "menu-link menu-toggle", // default submenu link class
+
+    "icon_class" => "menu-icon", // default menu icon class
+
+    "li_active_class" => "active", // default active class of li
+    "a_active_class" => "active", // default active class of a
+
+    "badge_class" => "badge rounded-pill ms-auto", // default badge class
+];
+```
+
 ## Advanced
 
 Each Item accept this parames :
--- config/menu.php
+++ config/menu.php
@@ -2,20 +2,20 @@
 
 // config for Salahhusa9/Menu, this is the default config
 return [
-    "ul_class" => "menu-inner py-1",
-    "ul_sub_menu_class" => "menu-sub",
+    "ul_class" => "menu-inner py-1", // default menu class
+    "ul_sub_menu_class" => "menu-sub", // default submenu class
 
-    "li_class" => "menu-item",
-    "li_sub_menu_class" => "menu-item",
-    "li_sub_menu_open_class" => "menu-item active open",
+    "li_class" => "menu-item", // default menu item class
+    "li_sub_menu_class" => "menu-item", // default submenu item class
+    "li_sub_menu_open_class" => "menu-item active open", // default submenu item class when open
 
-    "a_class" => "menu-link",
-    "a_sub_menu_class" => "menu-link menu-toggle",
+    "a_class" => "menu-link", // default menu link class
+    "a_sub_menu_class" => "menu-link menu-toggle", // default submenu link class
 
-    "icon_class" => "menu-icon",
+    "icon_class" => "menu-icon", // default menu icon class
 
-    "li_active_class" => "active",
-    "a_active_class" => "active",
+    "li_active_class" => "active", // default active class of li
+    "a_active_class" => "active", // default active class of a
 
-    "badge_class" => "badge rounded-pill ms-auto",
+    "badge_class" => "badge rounded-pill ms-auto", // default badge class
 ];

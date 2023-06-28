9d9df9c138fc0af0f2013d11bb0006655c3fc677
salahhusa9 <salahhusa9@gmail.com>
2023-06-28 12:54:56 +0100

    docs

--- src/Facades/Menu.php
+++ src/Facades/Menu.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static \Salahhusa9\Menu\Menu make(string $for, \Closure $callback) : \Salahhusa9\Menu\Menu
  * @method static \Salahhusa9\Menu\Menu add(string $title, $route_name,array $options) : \Salahhusa9\Menu\Menu
  * @method static \Salahhusa9\Menu\Menu addItem(string $title, $route_name,array $options) : \Salahhusa9\Menu\Item
+ * @method static \Salahhusa9\Menu\Menu addSubmenu(string $title, \Closure $callback) : \Salahhusa9\Menu\Menu
+ * 
  */
 class Menu extends Facade
 {
